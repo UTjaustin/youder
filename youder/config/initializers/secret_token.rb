9dee7981c0546cec628fdb03e10dd64877417f3b
Jerome A. Johnson <UTjaustin@gmail.com>
2014-02-02 19:46:09 -0700

    initial youder commit

---
+++ youder/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Youder::Application.config.secret_key_base = 'cea14cf66800ce074636c3c4bac6b95749b3e792d86e1c70758c1de3f70ec5b904c361b5920b03fdc1349af24cbc8a4b228a642c923b1a4c1315f362f0285fe4'
